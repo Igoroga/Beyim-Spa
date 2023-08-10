fff46f5627d14d5f49a793e193afc29ac6169342
Igor Gusakov <igorgusakov199918@gmail.com>
2023-08-10 19:57:13 +0600

    second

--- src/components/BodyMiddle.tsx
+++ src/components/BodyMiddle.tsx
@@ -1,16 +1,24 @@
 import React from 'react';
+import imgPlatform2 from '../image/imgPlatform2.png';
+import aboutPlatform from '../image/aboutPlatform.png';
+import imgPlatform1 from '../image/imgPlatform1.png';
 
 const BodyMiddle = () => {
     return (
         <div className='bodyMiddleMain'>
-            <div>
-            <h1></h1>
-            <p></p>
-                
+          <div className='contentColumn'>
+            <h1 className='contentColumn1'>Уникальный контент</h1>
+            <p className='contentColumn2'>Контент в разных форматах, видео, картинки, СОР, СОЧ , задачи ( активити )</p>
+            <div className='imageRow'>
+              <img className='aboutPlatform' src={aboutPlatform} alt="imgPlatform2" />
+              <img className='imgPlatform1' src={imgPlatform1} alt="imgPlatform2" />
             </div>
-            <img src="" alt="" />
+          </div>
+          <div className='imageColumn'>
+            <img src={imgPlatform2} alt="imgPlatform2" />
+          </div>
         </div>
-    );
+      );
 };
 
 export default BodyMiddle;
---
+++ src/components/BodySlide.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BodySlide = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default BodySlide;
